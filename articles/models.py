68fab0a230f03582eb44161b5b8afa39021a2642
technoteam <>
2022-09-05 10:41:32 +0430

    first commit

---
+++ articles/models.py
@@ -0,0 +1,18 @@
+from datetime import date
+from turtle import title
+from django.db import models
+
+
+class Articles(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    # aks
+    # nevisande
+
+    def __str__(self):
+        return self.title
+    
+    def snippet(self):
+        return self.body[:21]+"..."
